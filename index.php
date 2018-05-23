af9e57458c3861577d6730e99c6a2c928e4a21f2
Capaverde <dudu_capaverde@terra.com.br>
2018-05-23 13:49:32 -0300

    ac

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 <head>
-<title>Super Hell - Promoting tolerance since 2018</title>
+<title>Breakthrough Society - Keepin' it free</title>
 </head>
 <body>
-<p>Hello, world!</p>
+<p><a href="/files/">/files/</a> - A mirror for various files found freely on the surface web</p>
+<p><a href="/b/">/b/</a> - A completely unmoderated textboard, open to all, where free speech truly reigns</p>
 </body>
 </html>
